734ef532b36a45c05b871f0db20209e530039e47
Yuno Kim <unokim1547@gmail.com>
2025-10-31 23:44:03 +0900

    feat: 판매시작 창 반응형 스타일

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react"
 import { Link, Outlet, useLocation, Navigate, useNavigate } from "react-router-dom"
 
 import AdminLogin from "../Components/Login/AdminLogin"
-import styles from "./AdminPage.module.css"
+// import styles from "./AdminPage.module.css"
+import styles from "./AdminPageTest.module.css"
+// import styles from "./AdminPageResponsive.module.css"
 
 const AdminPage = () => {
   console.log('AdminPage 렌더링');
@@ -36,9 +38,22 @@ const AdminPage = () => {
         <h1 className={styles.mainText}>관리자 페이지</h1>
         <button onClick={handleLogout} className={styles.logoutBtn}>로그아웃</button>
       </header>
-        <nav className={styles.tabNav}>
+        {/* <nav className={styles.tabNav}>
           <Link to={"/admin/breakfast"} className={`${styles.tabBtn} ${location.pathname === '/admin/breakfast' ? styles.active : ''}`}>조식 관리</Link>
           <Link to={"/admin/menu"} className={`${styles.tabBtn} ${location.pathname === '/admin/menu' ? styles.active : ''}`}>메뉴 관리</Link>
+        </nav> */}
+        <nav className={styles.nav}>
+          <ul className={styles["nav-bar"]}>
+            {/* <Outlet>을 사용하므로, 하위 경로에서도 탭이 활성화되도록 
+              'startsWidth'를 사용했습니다.
+            */}
+            <li className={location.pathname.startsWith('/admin/breakfast') ? styles.current : ''}>
+              <Link to={"/admin/breakfast"}>조식 관리</Link>
+            </li>
+            <li className={location.pathname.startsWith('/admin/menu') ? styles.current : ''}>
+              <Link to={"/admin/menu"}>메뉴 관리</Link>
+            </li>
+          </ul>
         </nav>
       {/* 관리자 페이지 내용 */}
       <main className={styles.content}>
