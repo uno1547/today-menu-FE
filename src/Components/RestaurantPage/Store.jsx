e35c6eba16f8f3ab85a011dc87ac06273380514f
Yuno Kim <unokim1547@gmail.com>
2025-11-01 22:13:55 +0900

    feat: asset img and hovereffect

--- src/Components/RestaurantPage/Store.jsx
+++ src/Components/RestaurantPage/Store.jsx
@@ -1,21 +1,32 @@
 // import style from "./Store.module.css"
 import style from "./StoreTest.module.css"
 import { useNavigate, useParams } from "react-router-dom"
-/*
+
+import tobagi from "../../assets/tobagi.png"
+import arirang from "../../assets/arirang.png"
+import umga from "../../assets/umga.jpg"
+import kang from "../../assets/kang.jpg"
+import ramen from "../../assets/ramen.jpg"
+import hongdae from "../../assets/hongdae.png"
+import gongdup from "../../assets/gongdup.png"
+import daehan from "../../assets/daehan.jpeg"
+import suomien from "../../assets/suomien.jpeg"
+import hazben from "../../assets/hazben.jpeg"
+
+
 const storeImgMap = {
-  "또바기육개장": "",
-  "아리랑핫도그": "엄가네사진경로",
-  "엄가네해장국": "크앙사진경로",
-  "크앙분식": "노량진사진경로",
-  "라면집": "라면사진경로",
-  "홍대쌀국수": "홍대쌀국수사진경로",
-  "공여사&덮다": "카레사진경로",
-  "대한짜장": "돈까스집사진경로",
-  "수오미엔": "육개장집사진경로",
-  "하즈벤": "학생회관사진경로",
-  "홍대쌀국수": "학생회관사진경로",
+  "공여사&덮다": gongdup,
+  "대한짜장": daehan,
+  "또바기육개장": tobagi,
+  "라면집": ramen,
+  "수오미엔": suomien,
+  "아리랑핫도그": arirang,
+  "엄가네해장국": umga,
+  "크앙분식": kang,
+  "하즈벤": hazben,
+  "홍대쌀국수": hongdae,
 }
-*/
+
 const Store = ({ store }) => {
   const navigate = useNavigate()
   // const {restaurant} = useParams() params 받아서 식당/가게 전체주소를 명시할수도있음
@@ -26,7 +37,8 @@ const Store = ({ store }) => {
   }
   return (
     <div className={style.store} onClick={() => {navigate(`${name}`)}}>
-      <img src={image ? image : ""} alt={name}/>
+      {/* <img src={image ? image : ""} alt={name}/> */}
+      <img src={storeImgMap[name]} alt={name}/>
       <span className={style.text}>{name}</span>
     </div>
   )
