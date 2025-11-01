250aa3c90dc9773930cfbde3e1abaace29034870
Yuno Kim <unokim1547@gmail.com>
2025-11-01 19:14:09 +0900

    feat: building별 식당 불러와서 표시하기

--- src/Components/RestaurantPage/Store.jsx
+++ src/Components/RestaurantPage/Store.jsx
@@ -5,13 +5,13 @@ import { useNavigate, useParams } from "react-router-dom"
 const Store = ({ store }) => {
   const navigate = useNavigate()
   // const {restaurant} = useParams() params 받아서 식당/가게 전체주소를 명시할수도있음
-  const {name, url} = store
+  const {name, image} = store
   const navigateHandler = () => {
 
   }
   return (
     <div className={style.store} onClick={() => {navigate(`${name}`)}}>
-      <img src={url} alt={name}/>
+      <img src={null} alt={name}/>
       <span className={style.text}>{name}</span>
     </div>
   )
